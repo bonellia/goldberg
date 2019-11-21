840d8c81ae8a564bf10089a71007447c91ff6dcd
Taha Yasin Erol <tahayasinerol@mizmer.com.tr>
2019-11-21 16:34:37 +0300

    Add attach, detach, list Board methods.

--- goldberg.py
+++ goldberg.py
@@ -25,13 +25,15 @@ class User:
 
 class Board:
     board_count = 0
-    board_name = "Unknown"    
-    
+    board_name = "Unknown"
+    user_list = []
+    board_list = []
     def __init__(self, x=1280, y= 720, board_name ="Board#0"):
         Board.board_count += 1
         self.board_name = board_name
         self.board_space = pymunk.Space()
         self.board_space.gravity = 0, -100
+        Board.board_list.append(self)
         print("A board has been created with the name", self.board_name, "and current board count is:", self.board_count)
     
     def addShape(self, shape, x, y):
@@ -45,6 +47,19 @@ class Board:
     
     def moveShape(self, shape, x, y):
         shape.position = x, y
+    
+    def attach(self, user):
+        self.user_list.append(user)
+        print("The user", user, "has been attached to the board", self)
+    
+    def detach(self, user):
+        self.user_list.remove(user)
+        print("The user", user, "has been detached from the board", self)
+
+    @staticmethod
+    def list():
+        for b in Board.board_list:
+            print("The board", b, "is available with the name", b.board_name)
 
 
 class Shape:
@@ -80,6 +95,7 @@ class Ball(Shape):
             self.ball_shape = pymunk.Circle(self.ball_body, 8)
             self.ball_shape.elasticity = 0.728
             self.ball_shape.friction = 0.51
+
         elif ball_type == "Marble":
             self.ball_moment = pymunk.moment_for_circle(5, 0, 5)
             self.ball_body = pymunk.Body(1, self.ball_moment)
@@ -136,40 +152,4 @@ class Segment(Shape):
             print("This shouldn't happen. Enter valid ball type!(Book, Domino)")
         super().__init__(self.segment_body, self.segment_shape)
 
-# Init tests.
-window = pyglet.window.Window(1280, 720, "Pymunk Tester", resizable=False)
-options = DrawOptions()
-# Create a user.
-user1 = User("Taha")
-user2 = User("Artun")
-# Create a board.
-board1 = Board(1280, 720, "TestBoard")
-# Create 2 marbles.
-marble1 = Ball('Bowling')
-marble2 = Ball('Bowling')
-# Add marbles into the board.
-board1.addShape(shape=marble1, x=250, y=550)
-board1.addShape(shape=marble2, x=350, y=350)
-# Create a segment.
-segment1 = Segment("Fixed")
-# Add segment into the board.
-board1.addShape(shape=segment1, x=50, y=50)
-
-# View the board using Pyglet
-@window.event
-def on_draw():
-    window.clear()
-    board1.board_space.debug_draw(options)
-
-def update(dt):
-    board1.board_space.step(dt)
-    for shape in board1.board_space.shapes:
-        if shape.body.position.y < -100:
-            board1.board_space.remove(shape.body, shape)
-            print(shape, "has been removed to save resources.")
-            print("Current shapes in the board:", board1.board_space.shapes)
-
-if __name__ == "__main__":
-    pyglet.clock.schedule_interval(update, 1.0/60)
-    pyglet.app.run()
 
